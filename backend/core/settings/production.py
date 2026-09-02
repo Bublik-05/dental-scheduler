7b75550f33c46664ab63fb805c62b0b734d064ac
Bublik-05 <pernebekabylaj@gmail.com>
2026-09-02 15:46:29 +0500

    production updated 4

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -7,6 +7,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     config('RAILWAY_PUBLIC_DOMAIN', default=''),
     '.railway.app',
+    '.onrender.com',
 ]
 
 DATABASES = {
@@ -17,20 +18,29 @@ DATABASES = {
 }
 
 FRONTEND_URL = config('FRONTEND_URL', default='')
+
+CORS_ALLOWED_ORIGINS = [FRONTEND_URL] if FRONTEND_URL else []
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"https://dental-scheduler.*\.vercel\.app",
 ]
-CORS_ALLOWED_ORIGINS = [FRONTEND_URL] if FRONTEND_URL else []
 CORS_ALLOW_CREDENTIALS = True
+CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'authorization',
+    'content-type',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 
 CSRF_TRUSTED_ORIGINS = [
     FRONTEND_URL,
-    'https://dental-scheduler-production-71a9.up.railway.app',
+    'https://dental-scheduler-bvay.onrender.com',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
